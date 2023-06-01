c9b06fc913c285a148b3c67ca0a6180c9acfc21f
“larijuva” <lari.juva@hoiwa.fi>
2023-06-01 15:09:09 +0300

    -

--- src/pages/Pricing/Pricing.js
+++ src/pages/Pricing/Pricing.js
@@ -11,80 +11,8 @@ export default function Pricing() {
             <main id='pricingMain'>
                 <article className='prose'>
                     <section>
-                        <div>
-                            <h2>Hieronta</h2>
-                        </div>
-                        <table className="table shadow-xl">
-                            <tbody>
-                                <tr>
-                                    <td className='bg-primary'>30min</td>
-                                    <td className='bg-primary'>35€</td>
-                                </tr>
-                                <tr>
-                                    <td className='bg-primary'>45min</td>
-                                    <td className='bg-primary'>40€</td>
-                                </tr>
-                                <tr>
-                                    <td className='bg-primary'>60min</td>
-                                    <td className='bg-primary'>45€</td>
-                                </tr>
-                                <tr>
-                                    <td className='bg-primary'>75min</td>
-                                    <td className='bg-primary'>55€</td>
-                                </tr>
-                                <tr>
-                                    <td className='bg-primary'>90min</td>
-                                    <td className='bg-primary'>65€</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </section>
-                    <section>
-                        <div>
-                            <h2>Kuivakuppaus</h2>
-                        </div>
-                        <table className="table shadow-xl">
-                            <tbody>
-                                <tr>
-                                    <td className='bg-primary'>15min</td>
-                                    <td className='bg-primary'>20€</td>
-                                </tr>
-                                <tr>
-                                    <td className='bg-primary'>30min</td>
-                                    <td className='bg-primary'>30€</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </section>
-                    <section>
-                        <div>
-                            <h2>Intialainen päähieronta</h2>
-                        </div>
-                        <table className="table shadow-xl">
-                            <tbody>
-                                <tr>
-                                    <td className='bg-primary'>45min</td>
-                                    <td className='bg-primary'>50€</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </section>
-                    <section>
-                        <div>
-                            <h2>Teippaukset</h2>
-                        </div>
-                        <table className="table shadow-xl">
-                            <tbody>
-                                <tr>
-                                    <td className='bg-primary'>15min</td>
-                                    <td className='bg-primary'>25€</td>
-                                </tr>
-                                <tr>
-                                    <td className='bg-primary'>Sis. hierontaan</td>
-                                    <td className='bg-primary'>15€</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <h1>Hinnasto</h1>
+                        <img src="https://i.ibb.co/r7YLwZp/IMG-0117.jpg" width={window.innerWidth < 1280 ? "75%" : "33%"} />
                     </section>
                 </article>
             </main>
